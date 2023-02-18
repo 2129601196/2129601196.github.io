b5c695a0a87e58ddd0ddac234421820ad197cf27
2129601196 <2129601196@qq.com>
2023-02-18 17:46:25 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,17 @@
 function start(){
-    var iframeid = document.getElementById("NetEase-CloudMusic")
-    iframeid.src ="about:blank"
-    document.getElementById("Window").style.display = 'none'
+    var iframeid = document.getElementById("NetEase-CloudMusic");
+    iframeid.src ="about:blank";
+    document.getElementById("Window").style.display = 'none';
 }
 function play(){
-    document.getElementById("Window").style.display = 'block'
-    var iframeid = document.getElementById("NetEase-CloudMusic") 
-    iframeid.src = "https://music.163.com/outchain/player?type=2&id="+document.getElementById("Songid").value+"&auto=1&height=66"
+    document.getElementById("Window").style.display = 'block';
+    var iframeid = document.getElementById("NetEase-CloudMusic");
+    iframeid.src = "https://music.163.com/outchain/player?type=2&id="+document.getElementById("Songid").value+"&auto=1&height=66";
 }
 function play_hide(){
-    document.getElementById("Window").style.display = 'none'
-    document.getElementById("NetEase-CloudMusic").src ="about:blank"
-    new Audio("https://music.163.com/song/media/outer/url?id="+document.getElementById("Songid").value+".mp3").play()
+    document.getElementById("Window").style.display = 'none';
+    document.getElementById("NetEase-CloudMusic").src ="about:blank";
+    new Audio("https://music.163.com/song/media/outer/url?id="+document.getElementById("Songid").value+".mp3").play();
 }
 function reload(){
     window.location.reload();
@@ -21,8 +21,6 @@ function download(){
     window.location.href = download_mp3;
 }
 function 歌曲(number){
-    document.getElementById("Songid").value = number
-    document.getElementById("Window").style.display = 'block'
-    var iframeid = document.getElementById("NetEase-CloudMusic") 
-    iframeid.src = "https://music.163.com/outchain/player?type=2&id="+document.getElementById("Songid").value+"&auto=1&height=66"
+    document.getElementById("Songid").value = number;
+    play();
 }
